fd12f3ffc4dc0dd65d67add6d546642b33cb1546
Gabriel Souza <ondarks360@gmail.com>
2020-04-13 10:34:33 -0300

    atualizando

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ if not os.path.isfile(file_name):
 # rooms.load_cookie(file_name=file_name)
 # rooms.search_room()
 #url_input_room = input("Enter Room Id")
-url_room = 'https://drrr.com/room/?id=EZYByMxFCh'  # .format(url_input_room)
+url_room = 'https://drrr.com/room/?id=gcQFtQWLh6'  # .format(url_input_room)
 
 
 # main
